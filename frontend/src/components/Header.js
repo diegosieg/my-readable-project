1f4f8abfbf862b7ad113e3caeaee4c4fbf164bd2
Diego Sieg <diegosieg@gmail.com>
2018-01-09 22:13:14 +1300

    general layout updates;

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -9,20 +9,25 @@ const Header = ({ categories }) => {
   return (
     <header className="c-app-header">
       <NavLink to="/">
-        <h1>hey yo!</h1>
+        <h1 className="c-app-header-title">hey yo!</h1>
       </NavLink>
       <ul>
-        <li className="item">
-          <NavLink to="/" className="item-all" exact activeClassName="selected">
-            All
+        <li className="c-nav-item">
+          <NavLink
+            to="/"
+            className="c-nav-item-all"
+            exact
+            activeClassName="selected"
+          >
+            All articles
           </NavLink>
         </li>
         {categories.map(category => (
-          <li key={category.path} className="item">
+          <li key={category.path} className="c-nav-item">
             <NavLink
               exact
               to={`/${category.path}`}
-              className={`item-${category.path}`}
+              className={`c-nav-item-${category.path}`}
               activeClassName="selected"
             >
               {category.name}
